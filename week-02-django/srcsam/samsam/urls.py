efc4111c8b3a6af94778f1c9014121021350a154
Onestar <hanbyul@Onestarui-MacBookPro.local>
2020-12-20 20:15:53 +0900

    Add week-02-django project
    started blog app

--- week-02-django/srcsam/samsam/urls.py
+++ week-02-django/srcsam/samsam/urls.py
@@ -1,4 +1,4 @@
-"""sample URL Configuration
+"""samsam URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'blog/^', include('blog.urls')),
+    url(r'^blog/', include('blog.urls')),
     url(r'^admin/', admin.site.urls),
 ]
