9448e976d374cf413a71f971de4fb93e42f0f118
Onestar <hanbyul@Onestarui-MacBookPro.local>
2020-12-30 19:09:50 +0900

    meg

--- week-02-django/srcsam/blog/views.py
+++ week-02-django/srcsam/blog/views.py
@@ -1,6 +1,24 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+# from random import randint
+from .models import Article
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    # random_number = randint(1, 10)
+    # return HttpResponse("Hello, world. {}".format(random_number))
+    # return HttpResponse("Hello, world. You're at the index.")
+
+    # name = "hanbyul"
+    # return render(request, "index.html", { "name" : name })
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title="hello",
+    #     contents="this is test",
+    #     view_count=0
+    # )
+
+    ctx = {
+        "article_list" : article_list
+    }
+    return render(request, "index.html", ctx)
